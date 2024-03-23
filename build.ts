64f2edc240fa2d209049378b718c2c7527a7a0bb
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2024-03-22 22:03:38 -0300

    feat: save

--- build.ts
+++ build.ts
@@ -1,5 +1,5 @@
 import { readdir } from "fs/promises"
-import { relative, resolve } from "path"
+import { parse, relative, resolve } from "path"
 
 console.log("----------------- Building the pages")
 await Bun.$`rm -rf ./build/`
@@ -20,40 +20,49 @@ const server = await Bun.build({
         build.onLoad({ filter: /\.(ts|tsx)$/ }, async args => {
           const content = await Bun.file(args.path).text()
 
-          const uses = content.match(/(?:^|\n|;)("use (client|server)";?)/)
-          if (!uses) return { contents: content } // If there are no directives, we let it be bundled
+          const directives = content.match(/(?:^|\n|;)("use (client|server)";?)/)
+          if (!directives) return { contents: content } // If there are no directives, we let it be bundled
 
           const { exports } = new Bun.Transpiler({ loader: "tsx" }).scan(content)
           if (exports.length === 0) return { contents: content } // If there are no exports, we also let it be bundled
 
-          const refs = exports.map(e => {
-            const path = `/${relative(".", args.path).replace("src", "build").replace(".tsx", ".js").replace(".ts", ".js")}`
-
-            return uses[2] === "server"
-              ? // If it is a server component, we add fields to a error-throwing function to avoid shipping the code to the client
-                `\n\nexport const ${e}=()=>{throw new Error("This should only run on the server")};${e}.$$typeof=Symbol.for("react.server.reference");${e}.$$id="${path}#${e}";${e}.$$bound=null;`
-              : `${e === "default" ? "export default {" : `export const ${e} = {`}$$typeof:Symbol.for("react.client.reference"),$$id:"${path}#${e}",$$async:true};`
-          })
-
-          return { contents: refs.join("\n\n") }
+          return {
+            contents: exports
+              .map(e => {
+                console.log(e, args.path, directives[2], createReference(e, args.path, directives[2]))
+                return createReference(e, args.path, directives[2])
+              })
+              .join("\n\n")
+          }
         })
       }
     }
   ]
 })
 
-console.log("Successful build?", server.success)
+const createReference = (e: string, path: string, directive: string) => {
+  console.log(e, path, directive)
+  const id = `/${relative(".", path).replace("src", "build").replace(/\..+$/, ".js")}#${e}` // React uses this to identify the component
+  const mod = `${e === "default" ? parse(path).base.replace(/\..+$/, "") : ""}_${e}` // We create a unique name for the component export
+
+  return directive === "server"
+    ? // In case the of a server components, we add properties to a mock up function to avoid shipping the code to the client
+      `const ${mod}=()=>{throw new Error("This function is expected to only run on the server")};${mod}.$$typeof=Symbol.for("react.server.reference");${mod}.$$id="${id}";${mod}.$$bound=null;${e === "default" ? `export{${mod} as default}` : `export {${mod} as ${e}}`};`
+    : `${e === "default" ? "export default {" : `export const ${e} = {`}$$typeof:Symbol.for("react.client.reference"),$$id:"${id}",$$async:true};`
+}
+
+console.log("Successful build?", server)
 console.log("----------------- Building the components")
 
-const components = (await readdir(resolve("src/components"), { recursive: true })).map(file =>
-  resolve("src/components", file)
-)
+const components = (await readdir(resolve("src"), { recursive: true }))
+  .filter((file: string) => file.match(/\..+$/g) && !(file.endsWith(".d.ts") || file.endsWith("page.tsx")))
+  .map((file: string) => resolve("src", file))
 
 const client = await Bun.build({
   target: "bun",
   splitting: true,
   external: ["react", "react-dom", "react-server-dom-esm"],
-  entrypoints: [resolve("src/_client.tsx"), resolve("src/_layout.tsx"), ...components],
+  entrypoints: components,
   outdir: resolve("build")
 })
 
