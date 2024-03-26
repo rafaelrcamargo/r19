7d4817b5c449f1fb6c0f8ba069ae19ab44b355c5
rafaelrcamargo <rafaelrakochinski@gmail.com>
2024-03-26 15:54:38 -0300

    feat: upgrades :)

--- utils.ts
+++ utils.ts
@@ -9,7 +9,7 @@ export const createReference = (e: string, path: string, directive: string) => {
 
   return directive === "server"
     ? // In case the of a server components, we add properties to a mock up function to avoid shipping the code to the client
-      `const ${mod}=()=>{throw new Error("This function is expected to only run on the server")};${mod}.$$typeof=Symbol.for("react.server.reference");${mod}.$$id="${id}";${mod}.$$bound=null;${e === "default" ? `export{${mod} as default}` : `export {${mod} as ${e}}`};`
+    `const ${mod}=()=>{throw new Error("This function is expected to only run on the server")};${mod}.$$typeof=Symbol.for("react.server.reference");${mod}.$$id="${id}";${mod}.$$bound=null;${e === "default" ? `export{${mod} as default}` : `export {${mod} as ${e}}`};`
     : `${e === "default" ? "export default {" : `export const ${e} = {`}$$typeof:Symbol.for("react.client.reference"),$$id:"${id}",$$async:true};`
 }
 
@@ -22,7 +22,8 @@ export const logger = (req: Request, _: unknown, next: Function) => (
   console.log(
     `\x1b[2m${new Date().toISOString().replace(/.*T|Z/g, "")}\x1b[0m`,
     `\x1b[33m${req.method}\x1b[0m`,
-    `\x1b[2m(${pad(req.headers["user-agent"] ?? "May be Node")})\x1b[0m`,
+    `\x1b[2m(${pad(req.headers["user-agent"] ?? "Unknown")})\x1b[0m`,
+    `${req.headers["host"]?.split(":")[1]}`,
     `\x1b[32m"${req.path}"\x1b[0m`
   ),
   next()
