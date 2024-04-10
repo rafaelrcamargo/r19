29b978366c573a9e17a88c63d7227c4e209e0c2c
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2024-04-09 22:57:32 -0300

    feat: updates to nav + form example

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -6,6 +6,10 @@ const Page = () => {
   return (
     <main className="m-4 border-4 border-dashed border-red-400 p-4">
       <Form action={signup} />
+
+      <nav className="mt-4 [&_a]:text-blue-500 [&_a]:underline">
+        Go back to: <a href="/">/</a>
+      </nav>
     </main>
   )
 }
