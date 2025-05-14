753dc468c64ba38f6062c5e495d1c79b675d3b36
rafael r. camargo <66796237+rafaelrcamargo@users.noreply.github.com>
2025-05-14 12:33:24 -0300

    feat: update prettier-plugin-pkg to version 0.19.1 and refactor code

--- utils.ts
+++ utils.ts
@@ -9,8 +9,20 @@ export const createReference = (e: string, path: string, directive: string) => {
 
   return directive === "server"
     ? // In case the of a server components, we add properties to a mock up function to avoid shipping the code to the client
-      `const ${mod}=()=>{throw new Error("This function is expected to only run on the server")};${mod}.$$typeof=Symbol.for("react.server.reference");${mod}.$$id="${id}";${mod}.$$bound=null;${e === "default" ? `export{${mod} as default}` : `export {${mod} as ${e}}`};`
-    : `${e === "default" ? "export default {" : `export const ${e} = {`}$$typeof:Symbol.for("react.client.reference"),$$id:"${id}",$$async:true};`
+      `const ${mod} = () => {
+        throw new Error("This function is expected to only run on the server")
+      };
+
+      ${mod}.$$typeof=Symbol.for("react.server.reference");
+      ${mod}.$$id="${id}";
+      ${mod}.$$bound=null;
+
+      export { ${mod} as ${e} };`
+    : `${e === "default" ? "export default {" : `export const ${e} = {`}
+          $$typeof:Symbol.for("react.client.reference"),
+          $$id:"${id}",
+          $$async:true
+        };`
 }
 
 // Pad a string to a certain length
